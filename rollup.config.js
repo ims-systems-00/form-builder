a14be7f88a7aee8512dc15b4843792c6585080d5
RiadHossain43 <riadhossain7464@gmail.com>
2024-12-13 18:25:34 +0600

    fix package json

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,40 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-
-export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [resolve(), commonjs(), typescript()],
-  external: ["react", "react-dom"],
-};
+import dts from "rollup-plugin-dts";
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.lib.json",
+        jsx: "react",
+      }),
+      resolve({ extensions: [".js", ".ts", ".tsx"] }),
+      commonjs(),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "esm",
+      },
+    ],
+    plugins: [dts()],
+  },
+];
